864012bc4b4f24e4a10a66e6d5ccd6afe23aa35b
Natsumi26 <marion.redon26@yahoo.fr>
2026-09-21 11:34:58 +0200

    Création des fichiers controllers et templetes

---
+++ src/main/java/com/square_game/front_square_game/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.square_game.front_square_game.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
