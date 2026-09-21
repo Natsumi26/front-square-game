19e1ca3b4cecf46009b7d590b970f7630fd85bd7
Natsumi26 <marion.redon26@yahoo.fr>
2026-09-21 11:17:40 +0200

    Initial commit

---
+++ src/main/java/com/square_game/front_square_game/FrontSquareGameApplication.java
@@ -0,0 +1,13 @@
+package com.square_game.front_square_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontSquareGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontSquareGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/square_game/front_square_game/FrontSquareGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.square_game.front_square_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontSquareGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
